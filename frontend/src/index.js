0c1c8734c8fe04d85516770d1810ea68e4418889
Manh Le <quang.manh@gmail.com>
2017-12-02 12:15:42 -0600

    Redux preparation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import reducers from './reducers'
+import {creatStore, createStore} from 'redux'
+import {Provider} from 'react-redux'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
