12451585e7d824b680af0eb651c4b2f1418a427e
Manh Le <quang.manh@gmail.com>
2017-12-18 20:06:13 -0600

    Refactored Category

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import Post from './Post'
+import SortToolbar from './SortToolbar'
 
 
-function Category(props){
-    const {category} = props;
-    return <h1 style={{display:"inline-block"}}>{category.name}</h1>    
+class Category extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            sortBy: ''
+        }
+    }
+
+    sortCommand=(by)=>{
+        this.setState({sortBy:by})
+    }
+
+    render() {
+        const {sortBy} = this.state;
+        const { category } = this.props;        
+        const posts = this.props.posts.filter(p=>p.category === category.path);
+
+        
+        return <div>
+            <ul class='grid'>
+                <li>
+                    <h1 style={{ display: "inline-block",margin:0 }}>{category.name}</h1>
+                </li>
+                <li style={{justifyContent:"flex-end"}}>
+                    <SortToolbar sortBy={sortBy} onSortCommand={this.sortCommand}/>
+                </li>
+            
+            </ul>
+            <Divider/>
+            {
+                posts.map(p =>
+                    <div key={p.id} style={{marginBottom:".5em"}}><Post post={p} /></div>
+                )                        
+            }
+        </div>
+    }
+}
+function mapStateToProps(state) {
+    return {        
+        posts: state.posts,
+        apiService: state.apiService
+    }
 }
 
-export default Category;
+
+export default connect(mapStateToProps)(Category);
--- frontend/src/components/Views/DefaultView.js
+++ frontend/src/components/Views/DefaultView.js
@@ -2,35 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Paper from 'material-ui/Paper';
 import Category from '../Category'
-import Post from '../Post'
-import Divider from 'material-ui/Divider';
-import SortToolbar from '../SortToolbar'
+
 class DefaultView extends Component {
     constructor(props){
-        super(props)
-        this.state={
-            sortBy: ''
-        }
+        super(props)        
     }
 
-
-    render() {
-        const {sortBy} = this.state;
+    render() {        
         const { categories, posts } = this.props;
         return <div>
             {
-                categories.map(c => <div key={c.path}>
-                    <Category category={c} />
-                    <SortToolbar sortBy={sortBy}/>
-                    <Divider/>                    
-                    {
-                        posts.filter(p=>p.category === c.path).map(p =>
-                            <div key={p.id} style={{marginBottom:".5em"}}><Post post={p} /></div>
-                        )
-                        
-                    }
-                </div>                
-                )
+                categories.map(c =><Category category={c} key={c.path}/>)
             }
         </div>
     }
