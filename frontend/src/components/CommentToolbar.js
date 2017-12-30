3afa05fd209739565de29f7747fe9c511e09dc76
Manh Le <quang.manh@gmail.com>
2017-12-29 18:36:32 -0600

    Integrated DelConfirm to CommentToolbar

--- frontend/src/components/CommentToolbar.js
+++ frontend/src/components/CommentToolbar.js
@@ -1,5 +1,5 @@
-import React,{Component} from 'react'
-import { Pumpkin,Emerald, Carrot, Alizarin, Abestos ,Silver } from './colors'
+import React, { Component } from 'react'
+import { Pumpkin, Emerald, Carrot, Alizarin, Abestos, Silver } from './colors'
 import IconButton from 'material-ui/IconButton'
 import Thumbsup from 'material-ui/svg-icons/action/thumb-up'
 import Thumbsdown from 'material-ui/svg-icons/action/thumb-down'
@@ -9,78 +9,83 @@ import Edit from 'material-ui/svg-icons/image/edit'
 import FlatButton from 'material-ui/FlatButton';
 import DelConfirmation from './DelConfirmation'
 
-const styles={
+const styles = {
     smallIcon: {
         width: 17,
         height: 17,
-      },
-    iconHoverColor:"red"
+    },
+    iconHoverColor: "red"
 }
 
-export default class CommentToobar extends Component{
+export default class CommentToobar extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             open: false
         }
     }
 
-    handleAction(type){
-        this.props.onAction(type)
+    handleConfimationAction=(act)=> {
+        this.setState({open:false});
+        act==="OK" && this.props.onAction("DELETE");
     }
 
-    render(){
-        const {comment,editing} = this.props
-        const {voteScore} = comment;
-        return (
+    render() {
+        const { comment, onAction, editing } = this.props
+        const { open } = this.state;
+        const { voteScore } = comment;
 
-            <ul className="grid">
-            <li>
-                <IconButton iconStyle={styles.smallIcon} tooltip="Up vote" onClick={()=>this.handleAction("THUMBSUP")} >
-                    <Thumbsup color={Abestos} hoverColor={Emerald}/>
-                </IconButton>
-                <span>{voteScore}</span>
-                <IconButton iconStyle={styles.smallIcon} tooltip="Down vote" onClick={()=>this.handleAction("THUMBSDOWN")}>
-                    <Thumbsdown color={Abestos} hoverColor={Emerald}/>
-                </IconButton>
-            </li>
-            <li style={{ justifyContent: "flex-end" }}>
-                {
-                    !editing && <IconButton 
-                    iconStyle={styles.smallIcon}
-                    label="Edit" 
-                    onClick={() => this.handleAction("EDIT")}>
-                        <Edit />
-                    </IconButton>
-                }
-                {
-                    editing && <IconButton 
-                        label="Save" 
-                        onClick={() => this.handleAction("SAVE")}
-                        iconStyle={styles.smallIcon}
-                    >
-                        <Save/>
-                    </IconButton>
-                }
-                {
-                    editing && <IconButton 
-                        iconStyle={styles.smallIcon}                
-                        label="Cancel" 
-                        onClick={() => this.handleAction("CANCEL")}>
-                        <Cancel color={Alizarin}/>
-                    </IconButton>
-                }
-    
-                <FlatButton disabled={editing} 
-                    onClick={()=>this.handleAction("DELETE")} 
-                    label="Delete" labelStyle={{color:Pumpkin, disabledColor:"white"}}
-                />         
-                
-            </li>
-            </ul>
+        return (
+            <div>
+                <DelConfirmation open={open} message="Are you sure to delete this comment?" onAction={this.handleConfimationAction} />
+                <ul className="grid">
+                    <li>
+                        <IconButton iconStyle={styles.smallIcon} tooltip="Up vote" onClick={() => onAction("THUMBSUP")} >
+                            <Thumbsup color={Abestos} hoverColor={Emerald} />
+                        </IconButton>
+                        <span>{voteScore}</span>
+                        <IconButton iconStyle={styles.smallIcon} tooltip="Down vote" onClick={() => onAction("THUMBSDOWN")}>
+                            <Thumbsdown color={Abestos} hoverColor={Emerald} />
+                        </IconButton>
+                    </li>
+                    <li style={{ justifyContent: "flex-end" }}>
+                        {
+                            !editing && <IconButton
+                                iconStyle={styles.smallIcon}
+                                label="Edit"
+                                onClick={() => onAction("EDIT")}>
+                                <Edit />
+                            </IconButton>
+                        }
+                        {
+                            editing && <IconButton
+                                label="Save"
+                                onClick={() => onAction("SAVE")}
+                                iconStyle={styles.smallIcon}
+                            >
+                                <Save />
+                            </IconButton>
+                        }
+                        {
+                            editing && <IconButton
+                                iconStyle={styles.smallIcon}
+                                label="Cancel"
+                                onClick={() => onAction("CANCEL")}>
+                                <Cancel color={Alizarin} />
+                            </IconButton>
+                        }
+
+                        <FlatButton disabled={editing}
+                            onClick={() => this.setState({ open: true })}
+                            label="Delete" labelStyle={{ color: Pumpkin, disabledColor: "white" }}
+                        />
+
+                    </li>
+                </ul>
+            </div>
         )
     }
 
-   
+
 }
