90ace4dfdf00140ddba9ad508b1cda04f554cc69
Manh Le <quang.manh@gmail.com>
2018-01-07 22:27:29 -0600

    Finished Login Form

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react'
-import IconButton from 'material-ui/IconButton'
-import FontIcon from 'material-ui/FontIcon';
+import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton';
 import { Alizarin, SunFlower, Emerald, Silver } from './colors'
 import PropTypes from 'prop-types';
-import FlatButton from 'material-ui/FlatButton/FlatButton';
+
 import Icon from '../icons/github.svg'
 
 
-  
+///https://api.github.com/users/manhqle
 
 export default class LoginPage extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            login: ""
+        }
     }
 
     render() {
-        return <div style={{textAlign:"center"}}>
+        const { login } = this.state;
+        return (
+            <div style={{ textAlign: "center" }}>
+                <div style={{ width: 250,display:"inline-block" }}>
+                    <TextField fullWidth={true}
+                        underlineStyle={{ borderColor: Silver }}
+                        underlineFocusStyle={{ borderColor: Alizarin }}
+                        floatingLabelText="Github's Username"
+                        value={login}
+                        onChange={(e) => this.setState({ login: e.target.value })}
 
-            <div>
-                <RaisedButton backgroundColor="#00BCD4" 
-                labelStyle={{color:"white"}}
-                label="Login with GitHub" 
-                icon={<img src={Icon} width={24} height={24} alt="github" />}/>
+                    />
+                    <br />
+                    <RaisedButton
+                        style={{ width: 200 }} disabled={login.length === 0} backgroundColor="#00BCD4"
+                        fullWidth={true}
+                        labelStyle={{ color: "white" }}
+                        label="Login with GitHub"
+                        icon={<img src={Icon} width={24} height={24} alt="github" />} />
+                    <div style={{ margin: 12 }}> OR </div>
+
+                    <RaisedButton backgroundColor="#00BCD4"
+                        fullWidth={true}
+                        labelStyle={{ color: "white" }}
+                        label="Continue as Annonymous" />
+                </div>
             </div>
-            <div style={{margin:12}}> OR </div>
-            <RaisedButton label="Login as Annonymous" />
-        </div>
+        )
     }
 }
